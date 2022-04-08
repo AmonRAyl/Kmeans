5b3490e7619cde4a557e2f36eb24998ca90603f3
1566576 <80157145+1566576@users.noreply.github.com>
2022-04-08 19:21:33 +0200

    Update distance.py

--- distance.py
+++ distance.py
@@ -4,5 +4,5 @@ for i in X:
     for j in C:
         deuc=math.sqrt((i[0]-j[0])**2+(i[1]-j[1])**2+(i[2]-j[2])**2)#suponiendo 3 dimensiones
         fila.append(deuc)
-    Mat.append(fila)
-return np.matrix(Mat)
+    Mat.append(np.array(fila))
+return Mat
