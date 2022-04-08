967cce3d9813bfe7a0373f7c1f6125e2abe2ec38
1566576 <80157145+1566576@users.noreply.github.com>
2022-04-08 20:59:10 +0200

    PD:Aleix
    PD

--- distance.py
+++ distance.py
@@ -1,8 +1,2 @@
-Mat=[]
-for i in X:
-    fila=[]
-    for j in C:
-        deuc=math.sqrt((i[0]-j[0])**2+(i[1]-j[1])**2+(i[2]-j[2])**2)#suponiendo 3 dimensiones
-        fila.append(deuc)
-    Mat.append(np.array(fila))
-return Mat
+deuc = cdist(X, C, 'euclidean')
+return deuc
