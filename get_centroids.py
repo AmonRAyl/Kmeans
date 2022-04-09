5d2a77a5956c603c900b5f21fd33ba7d42398eca
1566576 <80157145+1566576@users.noreply.github.com>
2022-04-09 14:53:50 +0200

    Update get_centroids.py

--- get_centroids.py
+++ get_centroids.py
@@ -1,18 +1,7 @@
 np.copyto(self.old_centroids,self.centroids)
-aux=[]
+List=[]
 for i in range(self.K):
-    sumax=0
-    sumay=0
-    sumaz=0
-    tot=0
-    for j in range(self.X.shape[0]):
-        if self.labels[j] == i:
-            sumax=sumax+self.X[j][0]
-            sumay=sumay+self.X[j][1]
-            sumaz=sumaz+self.X[j][2]
-            tot=tot+1
-    mediox=(sumax)/tot
-    medioy=(sumay)/tot
-    medioz=(sumaz)/tot
-    aux.append([mediox,medioy,medioz])
-self.centroids=np.array(aux)
+    PuntosK=np.where(self.labels==i)
+    Puntos=self.X[PuntosK]
+    List.append(np.mean(Puntos,axis=0))
+self.centroids=np.array(List)
