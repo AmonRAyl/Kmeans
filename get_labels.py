43cbaccac9d8ac1f4264a2bdb499414a49c4d0e8
Finalinho <80196437+Finalinho@users.noreply.github.com>
2022-04-09 13:22:54 +0200

    Update get_labels.py
    con np

--- get_labels.py
+++ get_labels.py
@@ -1,11 +1,2 @@
-Vect=[]
-Mat=distance(self.X,self.centroids)
-for i in Mat:
-    Minim=i.min()
-    vect = i;
-    for j in range(self.K):
-        if vect[j] == Minim:
-            index=j
-            break
-    Vect.append(index)
-self.labels=np.array(Vect)
+Mat = distance(self.X, self.centroids)
+self.labels = np.argmin(Mat, axis=1)
